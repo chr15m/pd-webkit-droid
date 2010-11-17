b742e46f3ea055eabec8dede3209bbc448c04ae8
mccormix <mccormix@513906ac-38a0-a81e-fa7d-80853510afb5>
2010-11-17 11:27:48 +0000

    Default to portrait mode and allow full screen on big devices.

--- trunk/android/launch-emulator.sh
+++ trunk/android/launch-emulator.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "$1" == "" ]
+then
+	device="phone"
+else
+	device="$1"
+fi
+
 #emulator -avd test_avd -sdcard example-sdcard.img &
-emulator -avd phone &
-adb logcat &
+emulator -avd $device &
+if [ "`pidof adb`" == "" ]
+then
+	adb logcat &
+fi
